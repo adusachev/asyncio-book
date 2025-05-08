81ab90764c78156452a7389fec2949fb2987b911
adusachev <adusachev@yandex.ru>
2025-05-08 22:56:25 +0300

    insert db data on containe rstartup

--- db/migration.sql
+++ db/migration.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE brand, product, product_color, product_size, sku
+
 create table if not exists brand (
     brand_id serial primary key,
     brand_name text not null
@@ -36,3 +38,7 @@ comment on table sku is 'конкретный предмет, выставлен
 INSERT INTO product_color VALUES (1, 'Blue'), (2, 'Black');
 INSERT INTO product_size VALUES (1, 'Small'), (2, 'Medium'), (3, 'Large');
 
+INSERT INTO brand (brand_name)
+SELECT 
+    substr(md5(random()::text), 1, 8)
+FROM generate_series(1, 10000) AS gs;
